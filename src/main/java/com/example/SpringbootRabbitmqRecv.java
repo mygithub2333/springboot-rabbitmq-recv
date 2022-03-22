14923c1e9ca9d2e5bb823811b5e290568b2f7a9a
fuwenxin <1281370249@qq.com>
2022-03-22 18:00:53 +0800

    fix：rbmq消费者

---
+++ src/main/java/com/example/SpringbootRabbitmqRecv.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fwx
+ * @date 2022/3/22
+ */
+@SpringBootApplication
+public class SpringbootRabbitmqRecv {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqRecv.class,args);
+    }
+}
